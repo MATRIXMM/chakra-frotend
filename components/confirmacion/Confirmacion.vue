3272ce9981ef394e1ce7ac5ea3134cad2b277bb7
Marco Antonio Quezada Aquino <m.quezada@pucp.edu.pe>
2021-09-20 14:37:31 -0500

    feat: confirmacion agregada

---
+++ components/confirmacion/Confirmacion.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog :value="value" @input="$emit('input')" width="650">
+    <v-card class="mx-auto" height="220">
+      <v-card-title class="v-container-tittle">AGREGAR UN DOCUMENTO</v-card-title>
+      <v-card-text>
+        texto
+      </v-card-text>
+      <v-card-actions>
+        botones
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import {mdiAlert, mdiCheckboxMarkedCircle, mdiCloseCircle} from "@mdi/js";
+
+export default {
+  props: {
+    value: Boolean,
+
+  },
+  name: "Confirmacion",
+  data: () => ({
+    icons: {
+      mdiCloseCircle,
+      mdiCheckboxMarkedCircle,
+      mdiAlert
+    },
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/veterinario/index.vue
+++ pages/veterinario/index.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
-    <CuidadoSanitario />
-
+    <Confirmacion :key="dialogConfirmacionKey" v-model="dialogConfirmacion"@accept="RegistroInformacion" />
+    <v-btn
+      @click="RegistraDatos"
+    >
+      Clickea para el dialog
+    </v-btn>
   </div>
 </template>
 
 <script>
 
-import CuidadoSanitario from "@/components/veterinario/configuracion/CuidadoSanitario";
-
+import Confirmacion from "@/components/confirmacion/Confirmacion";
 
 export default {
   name: "index",
   components: {
-    CuidadoSanitario: CuidadoSanitario,
+    Confirmacion: Confirmacion,
   },
   data: () => ({
+    dialogConfirmacionKey: 1,
+    dialogConfirmacion: false,
   }),
   methods: {
+    RegistroInformacion(value){
+      console.log("Registro exitoso");
+    },
+    RegistraDatos(){
+      this.dialogConfirmacion = true;
+    },
   },
 }
 </script>
