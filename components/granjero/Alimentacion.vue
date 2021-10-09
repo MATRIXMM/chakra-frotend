c65eb0071d9332d31b1fe2038c17f989755dc8e4
Marco Antonio Quezada Aquino <m.quezada@pucp.edu.pe>
2021-10-08 23:03:04 -0500

    feat: componente alimentacion con card falta iteracion

--- components/granjero/Alimentacion.vue
+++ components/granjero/Alimentacion.vue
@@ -20,14 +20,30 @@
         ></v-select>
       </v-col>
     </v-row>
+    <v-chip color="blue" class="font-weight-bold" style="margin-top: 30px">
+      ALIMENTACION
+    </v-chip>
+    <v-container>
+      <CardAlimentacion />
+    </v-container>
+    <v-chip color="blue" class="font-weight-bold" style="margin-top: 30px">
+      ALIMENTACION ESPECIAL
+    </v-chip>
+    <v-container>
+      Contenido alimentacion especial
+    </v-container>
   </div>
 </template>
 
 <script>
 import {mapState} from "vuex";
+import CardAlimentacion from "@/components/granjero/CardAlimentacion";
 
 export default {
   name: "Alimentacion",
+  components: {
+    CardAlimentacion: CardAlimentacion,
+  },
   data: () => ({
     currentDay: new Date(),
     periodos: ['2021-2'],
---
+++ components/granjero/CardAlimentacion.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Familia 1
+    </v-card-title>
+    <v-card-text>
+      Horarios y datos
+    </v-card-text>
+    <v-card-actions>
+      botones
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "CardAlimentacion"
+}
+</script>
+
+<style scoped>
+
+</style>
