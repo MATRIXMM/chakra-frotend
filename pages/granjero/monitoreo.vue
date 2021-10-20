020332d34994808e3d4c9612a08b00bba894f932
MATRIXMM <m.quezada@pucp.edu.pe>
2021-10-20 01:26:20 -0500

    farmer pages

---
+++ pages/granjero/monitoreo.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <h2 class="headline">Monitoreo de las familias que realizan actividad de pastoreo</h2>
+  <p>A continuación, visualiará la ubicación de las familias de ganado</p>
+    <v-divider class="mt-2 mb-4"></v-divider>
+</div>
+</template>
+<script>
+export default {
+  layout: "main",
+
+}
+</script>
